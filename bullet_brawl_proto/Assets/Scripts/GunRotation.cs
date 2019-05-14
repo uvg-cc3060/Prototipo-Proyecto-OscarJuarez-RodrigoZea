bdf90d69b8eb6601fdbb284059323d3c677ad98c
OJP98 <ojuarez_p@hotmail.com>
2019-05-14 00:51:42 -0600

    Ajustes al apuntar

--- bullet_brawl_proto/Assets/Scripts/GunRotation.cs
+++ bullet_brawl_proto/Assets/Scripts/GunRotation.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class GunRotation : MonoBehaviour
 {
     public float speed = 5f;
+    private float angleCorrection = 90;
 
     // La pistola activamente deberia estar buscando la posicion del mouse o la stick del jugador
     void Update()
     {
+
+    	// Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    	// float midpoint = (transform.position - Camera.main.transform.position).magnitude * 1f;
+     // 	transform.LookAt(mouseRay.origin + mouseRay.direction * midpoint);
+
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - angleCorrection;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
-        //Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        //Quaternion rotation = Quaternion.LookRotation(direction);
-        //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
-        Debug.Log(transform.rotation);
+
+
+        // Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        // Quaternion rotation = Quaternion.LookRotation(direction);
+        // transform.rotation = rotation;
     }
 }
